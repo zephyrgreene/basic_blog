1f3dae7226d2323c13af8c75fbb05c436949df0b
zephyr <zephygreene@gmail.com>
2017-02-17 00:26:05 -0700

    Finished crud setup 2/2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,18 @@ class ArticlesController < ApplicationController
 		@article  = Article.new(article_params) 
 		@article.title
 		@article.save
+	
+		flash.notice = "Article '#{@article.title}' Created!"
+
+		redirect_to article_path(@article)
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title}' Updated!"
+
 		redirect_to article_path(@article)
 	end
 
@@ -27,6 +39,9 @@ class ArticlesController < ApplicationController
 
 	def destroy
 		@article = Article.find(params[:id]).destroy
+
+		flash.notice = "Article '#{@article.title}' Removed!"
+
 		redirect_to articles_path
 	end
 
