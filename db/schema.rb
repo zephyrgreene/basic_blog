71937d0700afdc08f03f38a03d4616e199f5d832
zephyr <zephygreene@gmail.com>
2017-02-20 11:52:38 -0700

    add tags, taggings models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220175400) do
+ActiveRecord::Schema.define(version: 20170220184412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,5 +31,21 @@ ActiveRecord::Schema.define(version: 20170220175400) do
     t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_taggings_on_article_id", using: :btree
+    t.index ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "comments", "articles"
+  add_foreign_key "taggings", "tags"
 end
