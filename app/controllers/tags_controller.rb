33281bdbe39ea6eb3108d533c51c7c462f4f0731
zephyr <zephygreene@gmail.com>
2017-02-22 00:40:31 -0700

    list tags -- completed, delete tags -- unfinished

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,21 @@
 class TagsController < ApplicationController
+
+  def index
+    @tags = Tag.all
+  end
+
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def destroy #commented out for now
+    # @tag = Tag.find(params[:id])
+    # @tag.articles.find(params[:article_id])}
+    # @tag.articles.delete(@article)
+
+    # flash.notice = "Subject '#{@tag.name}' Removed from all articles."
+  
+    redirect_to tags_path
+  end
+
 end
