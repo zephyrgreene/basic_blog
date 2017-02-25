12fe81aed17ce66c93d1c445b80a06100e0a363a
zephyr <zephygreene@gmail.com>
2017-02-25 15:31:10 -0700

    tag.destroy removed

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -8,14 +8,13 @@ class TagsController < ApplicationController
     @tag = Tag.find(params[:id])
   end
 
-  def destroy #commented out for now
-    # @tag = Tag.find(params[:id])
-    # @tag.articles.find(params[:article_id])}
-    # @tag.articles.delete(@article)
+  # def destroy
+  #   @tag = Tag.find(params[:id])
+  #   @tag.article.destroy
 
-    # flash.notice = "Subject '#{@tag.name}' Removed from all articles."
+  #   flash.notice = "Subject '#{@tag.name}' Removed from all articles."
   
-    redirect_to tags_path
-  end
+  #   redirect_to tags_path
+  # end
 
 end
